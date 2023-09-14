13544530f16b269049e56d05efaf5f2731feb347
GotWrecked <victorworobec@gmail.com>
2023-09-14 15:24:32 +1000

    Animated stars background

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,14 @@
 </script>
 
 <template>
+  <body style="background-color: black;">
+    <div class="bg-animation">
+        <div id="stars"></div>
+        <div id="stars2"></div>
+        <div id="stars3"></div>
+        <div id="stars4"></div>
+    </div>
+  </body>
   <Navbar />
   <RouterView />
 </template>
