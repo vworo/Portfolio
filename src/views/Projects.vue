988a74eb7d0ae0e19f144db14086d0208b0d3862
GotWrecked <victorworobec@gmail.com>
2023-09-26 05:04:59 +1000

    Added github and live external linking for Projects

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -9,6 +9,12 @@
         <div class="text-content">
             <h3>GameCade</h3>
             <p>This is a drawing game app built with React and Firebase. It allows multiple users to chat, and to create and join lobbies, where they can then select a game mode, and start drawing or interacting with other users depending on the game mode.</p>
+            <a href="https://gamecade-react.web.app/" target="_blank" rel="noopener">
+                <button>Live Site</button>
+            </a>
+            <a href="https://github.com/vworo/GameCade-Client-React" target="_blank" rel="noopener">
+                <button>Github</button>
+            </a>
         </div>
         <div class="image-content">
             <img src="../assets/SSGameCade.png" alt="GameCade lobby page with chatbox open">
@@ -19,6 +25,12 @@
         <div class="text-content">
             <h3>Beat Box</h3>
             <p>A web application that mimics the functionality of Spotify using Spotify's OAuth system. Users can authenticate with their Spotify accounts and access features such as browsing playlists, searching for songs, and playing music (click on a song to listen).</p>
+            <a href="https://boxbeats.netlify.app/" target="_blank" rel="noopener">
+                <button>Live Site</button>
+            </a>
+            <a href="https://github.com/vworo/Beat-Box" target="_blank" rel="noopener">
+                <button>Github</button>
+            </a>
         </div>
         <div class="image-content">
             <img src="../assets/SSBeatBox.png" alt="BeatBox Trending playlists page">
@@ -29,9 +41,15 @@
         <div class="text-content">
             <h3>Armory</h3>
             <p>An app designed for police and military inventory management. Requires login to access database usage.</p>
+            <a href="https://armory.fly.dev/" target="_blank" rel="noopener">
+                <button>Live Site</button>
+            </a>
+            <a href="https://github.com/vworo/Armory" target="_blank" rel="noopener">
+                <button>Github</button>
+            </a>
         </div>
         <div class="image-content">
-            <img src="../assets/SSArmory.png" alt="Armory ">
+            <img src="../assets/SSArmory.png" alt="Armory app showcasing sample data">
         </div>
     </div>
 
@@ -39,9 +57,15 @@
         <div class="text-content">
             <h3>Tic Tac Toe</h3>
             <p>A computer recreation of the famous 'Tic Tac Toe' game. Player 1 is assigned 'x' and will always go first in a game. Click 'Settings' to change grid size.</p>
+            <a href="https://vworo.github.io/Tic-Tac-Toe/" target="_blank" rel="noopener">
+                <button>Live Site</button>
+            </a>
+            <a href="https://github.com/vworo/Tic-Tac-Toe" target="_blank" rel="noopener">
+                <button>Github</button>
+            </a>
         </div>
         <div class="image-content">
-            <img src="../assets/SSTicTacToe.png" alt="BeatBox Trending playlists page">
+            <img src="../assets/SSTicTacToe.png" alt="Tic tac toe game grid screen">
         </div>
     </div>
 </template>
@@ -68,6 +92,7 @@
     img {
         max-width: 100%;
         height: auto;
+        border: solid 1px darkorange;
     }
 
     @media screen and (max-width: 600px) {
