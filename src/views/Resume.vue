b7b9691f0843326aeff0310802adc00efd6e4038
GotWrecked <victorworobec@gmail.com>
2023-09-26 10:09:50 +1000

    Changed embed to object due to phone development

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -3,17 +3,21 @@
 </script>
 
 <template>
-    <embed src="/Resume - LinkedIn.pdf" type="application/pdf" width="100%" height="760px">
+    <object data="/Resume - LinkedIn.pdf" type="application/pdf">
+        <p>Your browser does not support PDFs.</p>
+    </object>
 </template>
 
 <style scoped>
-    embed {
+    object {
         border-radius: 2em;
         border: 1px solid darkorange;
+        width: 100%;
+        height: 760px;
     }
 
     @media screen and (max-width: 600px) {
-        embed {
+        object {
             height: 575px;
         }
     }
