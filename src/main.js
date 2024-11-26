6f267ba6694bd71df3dc9819ab74b248c20a4bd0
vworo <victorworobec.gmail.com>
2024-11-27 01:49:13 +1100

    Setup bootstrap-vue-next with auto component registering

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import { createApp } from 'vue'
+
+// https://oh-vue-icons.js.org/docs
 import { OhVueIcon, addIcons } from "oh-vue-icons";
 import { LaHomeSolid, MdPersonoutline, MdLibrarybooksOutlined, HiDocumentText, HiMail } from "oh-vue-icons/icons";
+
+// https://bootstrap-vue-next.github.io/bootstrap-vue-next/
+import {createBootstrap} from 'bootstrap-vue-next'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 
 import App from './App.vue'
 import router from './router'
@@ -14,6 +21,7 @@ const app = createApp(App)
 
 // Make sure to _use_ the router instance to make the whole app router-aware.
 app.use(router)
+app.use(createBootstrap())
 
 app.component("v-icon", OhVueIcon);
 app.mount('#app')
